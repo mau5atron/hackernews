295c5f9f3af23a2c6cd48bbcfd3b3ea9722579e4
mau5atron <gabrielbetancourt96@outlook.com>
2018-07-03 22:21:13 -0400

    initialized state on list

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+);
 registerServiceWorker();
+
+// this is a hot module replacement tool
+// instead of the browser performing a refresh(reload), the application reloads instead and shows the changed state
+// HMR keeps application state intact even though the source code changes
+// will become useful in larger applications with lots of assets to reload
+if (module.hot){
+	module.hot.accept();
+}
