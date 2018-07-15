72aaaaea74268d2b7df4e6f4b751242cee152f58
mau5atron <gabrielbetancourt96@outlook.com>
2018-07-15 17:25:25 -0400

    made table component to concise functional stateless component

--- src/App.js
+++ src/App.js
@@ -106,31 +106,26 @@ const Search = ({value, onChange, children}) =>
 	</form>
 
 
-class Table extends Component {
-	render(){
-		const { list, pattern, onDismiss } = this.props;
-		return(
-		<div>
-    	{list.filter(isSearched(pattern)).map(item => 
-    			<div key={item.objectID}>
-    				<span>
-    					<a href={item.url}>{item.title}</a>
-    				</span>
-    				<span>{item.author}</span>
-    				<span>{item.num_comments}</span>
-    				<span>{item.points}</span>
-    				<span>
-    					<button onClick={() =>
-    						onDismiss(item.objectID)} type="button">Dismiss
-    					</button>
-    				</span>
-    				<hr/>
-    			</div>
- 			)}
-    </div>
-		);
-	}
-}
+const Table = ({list, pattern, onDismiss}) => 
+		// const { list, pattern, onDismiss } = this.props;
+	<div>
+  	{list.filter(isSearched(pattern)).map(item => 
+  			<div key={item.objectID}>
+  				<span>
+  					<a href={item.url}>{item.title}</a>
+  				</span>
+  				<span>{item.author}</span>
+  				<span>{item.num_comments}</span>
+  				<span>{item.points}</span>
+  				<span>
+  					<button onClick={() =>
+  						onDismiss(item.objectID)} type="button">Dismiss
+  					</button>
+  				</span>
+  				<hr/>
+  			</div>
+			)}
+  </div>
 
 class Button extends Component {
 	render(){
